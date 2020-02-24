4731ed001b2a8c8c6711a50239e77e6025621f0a
Chechobuitrago <50975995+Chechobuitrago@users.noreply.github.com>
2020-02-24 16:53:21 -0500

    Controll

---
+++ Kruunker-Ahorcado_Server/src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,9 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.view.VentanaPrincipal;
+
+public class Controller {
+	public Controller() {
+		VentanaPrincipal vp = new VentanaPrincipal(this);
+	}
+}
